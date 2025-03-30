3afe8b2a64bd7d721005e74af77ec98b71f2a7c0
mmsxy <ql2677@nyu.edu>
2025-03-30 13:26:16 -0400

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,11 @@
+import BusinessSimulationForm from "./components/BusinessSimulationForm";
+
+function App() {
+  return (
+    <div className="App">
+      <BusinessSimulationForm />
+    </div>
+  );
+}
+
+export default App;
